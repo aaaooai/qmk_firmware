74d5bfe85fa775fcba62bfef9c77512b4957185c
ﾀﾅｶ ﾄﾓｱｷ <minazukijiro@gmail.com>
2026-04-10 09:33:48 +0900

    update keymap

--- keyboards/crkbd/keymaps/cmdh/keymap.c
+++ keyboards/crkbd/keymaps/cmdh/keymap.c
@@ -18,76 +18,31 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
-//#include "qmk_userspace/users/naginata_v17/naginata.h"
-
 enum layouts {
-  _QWERTY = 0,
-  _COLEMAK_DH,
+  _COLEMAK_DH = 0,
   _LOWER,
   _RAISE,
   _ADJUST,
+  //  _NAGINATA,
 };
 
-#define QWERTY TO(_QWERTY)
-#define CMDH   TO(_COLEMAK_DH)
-#define LOWER  MO(_LOWER)
-#define RAISE  MO(_RAISE)
-#define ADJUST MO(_ADJUST)
-
-#define KC_SPOT LGUI(KC_SPC)
-#define KC_PSCR LGUI(LSFT(KC_5))
-
-#undef KC_COPY
-#define KC_COPY LCTL(KC_INS)
-
-#undef KC_PSTE
-#define KC_PSTE LSFT(KC_INS)
-
-#undef KC_CUT
-#define KC_CUT  LSFT(KC_DEL)
-
-#define KC_NXTW LGUI(KC_PGDN)
-#define KC_PRVW LGUI(KC_PGUP)
-
-#ifdef TAP_DANCE_ENABLE
-
-enum {
-  TD_PASS
-};
+#define CMDH    TO(_COLEMAK_DH)
+#define LOWER   MO(_LOWER)
+#define RAISE   MO(_RAISE)
+#define ADJUST  MO(_ADJUST)
 
 #if __has_include("secret.h")
   #include "secret.h"
   #define SECRET
 #endif
 
-void dance_pass_finished(tap_dance_state_t *state, void *user_data) {
-#ifdef SECRET
-  if (state->count > 1)
-    SEND_STRING(PASS_STR SS_TAP(X_ENT));
-#endif
-}
-
-tap_dance_action_t tap_dance_actions[] = {
-  [TD_PASS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_pass_finished, NULL)
+enum custom_keycodes {
+    KC_PASS = SAFE_RANGE,
+    /* KC_NAGI, */
+    /* KC_DENG, */
 };
 
-#define KC_PASS TD(TD_PASS)
-
-#endif  /* TAP_DANCE_ENABLE */
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_QWERTY] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   LOWER,  KC_SPC,     KC_ENT,   RAISE, KC_RALT
-                                      //`--------------------------'  `--------------------------'
-  ),
-
     [_COLEMAK_DH] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_BSPC,
@@ -98,18 +53,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   LOWER,  KC_SPC,     KC_ENT,   RAISE, KC_RALT
                                       //`--------------------------'  `--------------------------'
-
   ),
 
     [_LOWER] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_COPY,  KC_CUT,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
+      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSTE, XXXXXXX,                      XXXXXXX, KC_PASS, XXXXXXX, _______, _______, XXXXXXX,
+      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, _______, _______, _______, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, _______,       CMDH,  ADJUST,  QWERTY
+                                          _______, _______, _______,    _______,  ADJUST, _______
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -125,18 +79,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
   ),
 
+
     [_ADJUST] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       QK_BOOT,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______,  KC_F11,  KC_F12, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX, XXXXXXX,
+      _______,  KC_F11,  KC_F12, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_MUTE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_PASS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
   )
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (!record->event.pressed) return true;
+    switch (keycode) {
+    case KC_PASS:
+#ifdef SECRET
+      SEND_STRING(PASS_STR SS_TAP(X_ENT));
+      return false;
+#else
+      return true;
+#endif
+    }
+    return true;
+}
 
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
